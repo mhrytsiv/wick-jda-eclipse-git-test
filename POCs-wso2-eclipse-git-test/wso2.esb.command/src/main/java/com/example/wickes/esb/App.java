310cdfd8adb422b465b97f87b099ed682a993bad
Mykhaylo_Hrytsiv <Mykhaylo_Hrytsiv@epam.com>
2016-07-25 23:05:48 +0300

    Changed message in App class 2

--- POCs-wso2-eclipse-git-test/wso2.esb.command/src/main/java/com/example/wickes/esb/App.java
+++ POCs-wso2-eclipse-git-test/wso2.esb.command/src/main/java/com/example/wickes/esb/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Updated" );
+        System.out.println( "Hello World Updated 2" );
     }
 }
