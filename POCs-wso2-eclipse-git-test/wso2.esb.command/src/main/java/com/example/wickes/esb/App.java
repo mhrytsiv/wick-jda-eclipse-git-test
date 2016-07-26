cf2a42298248dfc36a7962162b8c5c4d8d1138a3
Mykhaylo_Hrytsiv <Mykhaylo_Hrytsiv@epam.com>
2016-07-26 13:20:21 +0300

    Implemented Feature 1

--- POCs-wso2-eclipse-git-test/wso2.esb.command/src/main/java/com/example/wickes/esb/App.java
+++ POCs-wso2-eclipse-git-test/wso2.esb.command/src/main/java/com/example/wickes/esb/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Updated 10000" );
+        System.out.println( "Hello World Updated Implemented Feature 1" );
     }
 }
--- POCs-wso2-eclipse-git-test/wso2.esb.command/src/main/java/com/example/wickes/esb/PojoCommand.java
+++ POCs-wso2-eclipse-git-test/wso2.esb.command/src/main/java/com/example/wickes/esb/PojoCommand.java
@@ -3,6 +3,6 @@ package com.example.wickes.esb;
 public class PojoCommand {
 
 	public static void main(String[] args) {
-		System.out.println("Inside PojoCommand 1000");
+		System.out.println("Inside PojoCommand Implemented Feature 1");
 	}
 }
