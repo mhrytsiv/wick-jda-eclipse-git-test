2f5be96c80cf9d2977401da7908dc9e319243b3e
Mykhaylo_Hrytsiv <Mykhaylo_Hrytsiv@epam.com>
2016-07-26 16:05:20 +0300

    Inside PojoCommand Simple Change

--- POCs-wso2-eclipse-git-test/wso2.esb.command/src/main/java/com/example/wickes/esb/PojoCommand.java
+++ POCs-wso2-eclipse-git-test/wso2.esb.command/src/main/java/com/example/wickes/esb/PojoCommand.java
@@ -3,6 +3,6 @@ package com.example.wickes.esb;
 public class PojoCommand {
 
 	public static void main(String[] args) {
-		System.out.println("Inside PojoCommand Implemented Feature 3");
+		System.out.println("Inside PojoCommand Simple Change");
 	}
 }
