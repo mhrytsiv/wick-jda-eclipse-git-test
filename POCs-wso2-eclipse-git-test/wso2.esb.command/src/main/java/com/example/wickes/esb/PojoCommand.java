70660cf4f4b53ffaec476177fa9d820e30030b32
Mykhaylo_Hrytsiv <Mykhaylo_Hrytsiv@epam.com>
2016-07-26 14:29:36 +0300

    Implemented Feature 3

--- POCs-wso2-eclipse-git-test/wso2.esb.command/src/main/java/com/example/wickes/esb/PojoCommand.java
+++ POCs-wso2-eclipse-git-test/wso2.esb.command/src/main/java/com/example/wickes/esb/PojoCommand.java
@@ -3,6 +3,6 @@ package com.example.wickes.esb;
 public class PojoCommand {
 
 	public static void main(String[] args) {
-		System.out.println("Inside PojoCommand Implemented Feature 2");
+		System.out.println("Inside PojoCommand Implemented Feature 3");
 	}
 }
