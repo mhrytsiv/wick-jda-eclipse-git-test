eef21839735d50c89026b76f5af10481dc5f44f5
Tymur_Hrytsiv <Tymur_Hrytsiv@epam.com>
2016-07-26 20:47:26 +0300

    Removed all messages from App.

--- POCs-wso2-eclipse-git-test/wso2.esb.command/src/main/java/com/example/wickes/esb/App.java
+++ POCs-wso2-eclipse-git-test/wso2.esb.command/src/main/java/com/example/wickes/esb/App.java
@@ -4,13 +4,7 @@ package com.example.wickes.esb;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	System.out.println( "Tymur change by mykhaylojavadev" );
-    	System.out.println( "Changed by mykhaylojavadev" );
-    	System.out.println( "Changed by mykhaylojavadev" );
-        System.out.println( "Hello World Updated Implemented Feature 3" );
-    }
+public class App {
+	public static void main(String[] args) {
+	}
 }
