81c3ef1eccb65ed65ae09f9dc21abfc14bd32acf
Mykhaylo_Hrytsiv <Mykhaylo_Hrytsiv@epam.com>
2016-07-26 22:38:23 +0300

    Added Executor, Removed Processor, Modified Reader and Writer.

--- POCs-wso2-eclipse-git-test/wso2.esb.command/src/main/java/wso2/esb/command/Writer.java
+++ POCs-wso2-eclipse-git-test/wso2.esb.command/src/main/java/wso2/esb/command/Writer.java
@@ -1,8 +1,17 @@
 package wso2.esb.command;
 
+import java.io.File;
+
 public class Writer {
 
 	public static void write() {
-		// TODO WRITE
+		final String fileName = "test.txt";
+		final File file = new File(fileName);
+		writeToFile(file);
 	}
+
+	public static void writeToFile(final File file) {
+		System.out.println("Write to file");
+	}
+
 }
