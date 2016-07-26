d034de45ad5305f04bf477bd7ee25c69cf09f877
Mykhaylo_Hrytsiv <Mykhaylo_Hrytsiv@epam.com>
2016-07-26 17:20:12 +0300

    Added by mykhaylojavadev

--- POCs-wso2-eclipse-git-test/wso2.esb.command/src/main/java/com/example/wickes/esb/App.java
+++ POCs-wso2-eclipse-git-test/wso2.esb.command/src/main/java/com/example/wickes/esb/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+    	System.out.println( "Added by mykhaylojavadev" );
         System.out.println( "Hello World Updated Implemented Feature 3" );
     }
 }
